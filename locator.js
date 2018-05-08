76f9743985c827373abe4eafb9609b452c4f5a24
patrick.greenwell <patrick.greenwell@quickpivot.com>
2018-05-08 12:58:20 -0400

    separate concerns

--- locator.js
+++ locator.js
@@ -1,37 +1,38 @@
 export default class Locator
 {
+     
     
+
     constructor()
     {
         var geo_options = {
             enableHighAccuracy: true, 
             maximumAge        : 5000, 
-            timeout           : 27000
+            timeout           : 2700
           };
           this.Positions = [];
         
-        this.wpid = navigator.geolocation.watchPosition(success,error,geo_options)
+        this.wpid = navigator.geolocation.watchPosition(position => {
+               
+            console.log(position.coords);
+            this.Positions.push(position.coords);
+        }, error=> {
+            switch (error.code) {
+                case 1:
+                    console.warn('Geo Permission denied, please give access')
+                    break;
+                case 2:
+                    console.info('Postion Unavailable')
+                    break;
+                case 3:
+                    console.info('Geolocation Timeout')
+                    break;
+                default:
+                    console.log(error);
+                    break;
+            }
+        },geo_options)
     }
-    success(position){
-        postion.timestamp = new Date();
-        console.log(position);
-        this.Positions.push(position);
-    };
-    error(positionerror){
-        switch (positionerror.code) {
-            case 1:
-                console.warn('Geo Permission denied, please give access')
-                break;
-            case 2:
-                console.info('Postion Unavailable')
-                break;
-            case 3:
-                console.info('Geolocation Timeout')
-                break;
-            default:
-                console.log(positionerror);
-                break;
-        }
-    };
+   
     
 }
